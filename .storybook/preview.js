edad86b6cd01a292719ffd564f001b8518465c80
Joshua Comeau <joshwcomeau@gmail.com>
2021-01-19 09:50:59 -0500

    Solve ProgressBar. Delete Button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
+import './global.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 export const COLORS = {
-  primary: 'hsl(240deg 100% 60%)',
-  primaryLight: 'hsl(235deg 100% 62%)',
-  white: 'hsl(0deg 0% 100%)',
-  offwhite: 'hsl(235deg 85% 97%)',
-  gray: 'hsl(240deg 10% 50%)',
-  transparentGray15: 'hsl(240deg 10% 50% / 0.15)',
-  transparentGray75: 'hsl(240deg 10% 50% / 0.75)',
+  primary: 'hsl(240deg 80% 60%)',
+  gray50: 'hsl(0deg 0% 95%)',
+  gray300: 'hsl(0deg 0% 75%)',
+  gray500: 'hsl(0deg 0% 50%)',
+  gray700: 'hsl(0deg 0% 40%)',
   black: 'hsl(0deg 0% 0%)',
+  transparentGray15: 'hsl(0deg 0% 50% / 0.15)',
+  transparentGray35: 'hsl(0deg 0% 50% / 0.35)',
 };
