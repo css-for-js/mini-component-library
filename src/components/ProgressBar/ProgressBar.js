7630b7e5bd783b42d60aacced0e3424fd167dae7
Annie Liao <liaowow@gmail.com>
2021-04-17 08:35:38 -0400

    create progressbar via html element

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,15 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <div 
+      role="progressbar" 
+      aria-valuenow={value} 
+      aria-valuemin="0" 
+      aria-valuemax="100">
+        <VisuallyHidden>{value}%</VisuallyHidden>
+    </div>
+  );
 };
 
 export default ProgressBar;
