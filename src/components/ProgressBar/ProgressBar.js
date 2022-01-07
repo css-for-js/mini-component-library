4154453667c0a2920a0bf1e540d73d272ce820c1
Barry May <5514034+barrymay@users.noreply.github.com>
2022-01-07 04:51:47 +0000

    Initial Progres bar size work

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,49 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled, { CSSProperties } from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  large: {
+    "--height": "24px",
+    "--padding": "4px",
+  },
+  medium: {
+    "--height": "12px",
+    "--padding": 0,
+  },
+  small: {
+    "--height": "8px",
+    "--padding": 0,
+  },
+};
+
+const OuterWrapper = styled.div`
+  width: 100%;
+  height: var(--height);
+  padding: var(--padding);
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+`;
+
+const InnerWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: ${COLORS.primary};
+  content: "";
+  border-radius: 4px;
+`;
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const selectedStyle = SIZES[size];
+  return (
+    <OuterWrapper style={selectedStyle}>
+      <InnerWrapper>&nbsp;</InnerWrapper>
+    </OuterWrapper>
+  );
 };
 
 export default ProgressBar;
