6a3c881a5b417429ed17a8f55f05efd4d98cd756
Barry May <5514034+barrymay@users.noreply.github.com>
2022-01-07 15:29:45 -0500

    Handle dynamic min/max

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,6 +5,9 @@ import styled, { CSSProperties } from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const MIN = 0;
+const MAX = 10;
+
 const SIZES = {
   large: {
     "--height": "24px",
@@ -22,9 +25,9 @@ const SIZES = {
 
 const OuterWrapper = styled.div.attrs((props) => ({
   role: "progressbar",
-  "aria-valuenow": `${props.value}`,
-  "aria-valuemin": "0",
-  "aria-valuemax": "100", // Every <Button /> will now have type="button" as default
+  "aria-valuenow": `${props["data-value"]}`,
+  "aria-valuemin": MIN,
+  "aria-valuemax": MAX, // Every <Button /> will now have type="button" as default
 }))`
   width: 100%;
   height: var(--height);
@@ -44,7 +47,7 @@ const ProgressMask = styled.div`
 
 const Progress = styled.div`
   height: 100%;
-  width: ${(props) => `${props.value}%`};
+  width: ${(props) => `${props["data-value"]}%`};
   transition: width 0.3s;
   background-color: ${COLORS.primary};
 `;
@@ -52,11 +55,16 @@ const Progress = styled.div`
 const ProgressBar = ({ value, size }) => {
   const selectedStyle = SIZES[size];
   const resolvedValue =
-    typeof value !== "number" || value < 0 ? 0 : value > 100 ? 100 : value;
+    typeof value !== "number" || value < MIN
+      ? 0
+      : value > MAX
+      ? 100
+      : 100 * ((value - MIN) / (MAX - MIN));
   return (
-    <OuterWrapper style={selectedStyle} value={resolvedValue}>
+    <OuterWrapper style={selectedStyle} data-value={value}>
       <ProgressMask>
-        <Progress value={resolvedValue} />
+        <Progress data-value={resolvedValue} />
+        <VisuallyHidden>{value}%</VisuallyHidden>
       </ProgressMask>
     </OuterWrapper>
   );
