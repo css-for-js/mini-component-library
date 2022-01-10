6de3ecb34aacdfdbba6627acbfc53c29754af6e3
Barry May <5514034+barrymay@users.noreply.github.com>
2022-01-10 04:57:25 +0000

    Supply limits to width control

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -10,52 +10,60 @@ const SIZES = {
   small: {
     lineWeight: 1,
     inputLeft: 20,
+    inputHeight: 24 / 16,
     iconSize: 16,
     fontSize: 14 / 16,
-    fontWeight: 400,
   },
 
   large: {
     lineWeight: 2,
     inputLeft: 30,
+    inputHeight: 36 / 16,
     iconSize: 24,
     fontSize: 18 / 16,
-    fontWeight: 700,
+
   },
 };
 
 const Label = styled.label`
   display: block;
   position: relative;
+  
   &:focus {
     outline: 5px auto black;
   }
+
 `;
 
-const Input = styled.input`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  border-left: 0;
-  border-top: 0;
-  border-right: 0;
-  outline-offset: 3px;
+const TextInput = styled.input`
+  height: var(--input-height);
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
   padding-left: var(--input-left);
-  font-weight: var(--font-weight);
+  font-weight: 700;
   font-size: var(--font-size);
+  outline-offset: 1px;
+  color: ${COLORS.gray700};
+  width: ${props => `${props.width}px`};
+  &:hover {
+    color: ${COLORS.black}
+  }
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
 `;
 
-const IconWrapper = styled(Icon)`
+const IconWrapper = styled.div`
   position: absolute;
   top: 0;
-  left: 0;
+  bottom: 0;
+  margin: auto 0;
   width: var(--icon-size);
   height: var(--icon-size);
-  padding-bottom: 4px;
 `;
 
-const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
   const style = SIZES[size];
   if (!style) {
     throw new Error("Improper style passed");
@@ -64,14 +72,18 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
     <Label
       style={{
         "--line-weight": style.lineWeight + "px",
+        "--input-height": style.inputHeight + "rem",
         "--input-left": style.inputLeft + "px",
         "--font-size": style.fontSize + "rem",
         "--font-weight": style.fontWeight,
-        "--icon-size": style.iconSize,
+        "--icon-size": style.iconSize + "px",
       }}
     >
-      <Input type="text" />
-      <IconWrapper id={icon} size={style.iconSize} />
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <TextInput  type="text" width={width} {...delegated} />
+      <IconWrapper>
+        <Icon id={icon} size={style.iconSize} />
+      </IconWrapper>
     </Label>
   );
 };
