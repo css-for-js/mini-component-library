abbe881757cd460e096c092bf11cba9dfd30fd32
jy-ko <jclareko@gmail.com>
2023-04-13 15:43:15 +0300

    progress bar complete

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,20 +6,62 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const SIZE = {
-  "L": "24px", 
+  "large": "24px", 
   "medium": "12px", 
   "small": "8px"
 }
+
+const STYLES = {
+  small: {
+    height: 8, 
+    padding: 0,
+    radius: 4,
+  }, 
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large : {
+    height: 16,
+    padding: 4, 
+    radius: 8
+  }
+}
 const Wrapper  = styled.div`
   background-color: ${COLORS.transparentGray15};
-  height: ${props => SIZE[props.size]};
-  width: 370px;
-  border-radius: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray15};
+  border-radius: var(--radius);
+  padding: var(--padding);
 `
-const ProgressBar = ({ value, size }) => {
-  return (
-    <Wrapper size={size}>
+const InnerBar = styled.div`
+ height: var(--height);
+ background-color: ${COLORS.primary};
+ border-radius: 4px 0 0 4px;
+ width: var(--width);
+`
 
+const BarWrapper = styled.div`
+ border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`
+const ProgressBar = ({ value = 0, size }) => {
+  const styles = STYLES[size];
+  if (!styles) {
+    throw new Error(`Unknown Size passed to ProgressBar: ${size}`);
+  }
+  return (
+    <Wrapper size={size} role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px', 
+        '--radius': styles.radius + 'px'
+      }}
+      >
+      <InnerBar size={size} style={{ '--width': value + "%", '--height': styles.height + 'px'}}/>
     </Wrapper>
   )
 };
