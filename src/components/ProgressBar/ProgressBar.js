8cb486610f7654dfd39659fca8ab9952b3c5a130
jy-ko <jclareko@gmail.com>
2023-04-13 15:02:53 +0300

    added sizes

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,23 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZE = {
+  "L": "24px", 
+  "medium": "12px", 
+  "small": "8px"
+}
+const Wrapper  = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  height: ${props => SIZE[props.size]};
+  width: 370px;
+  border-radius: 8px;
+`
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper size={size}>
+
+    </Wrapper>
+  )
 };
 
 export default ProgressBar;
--- src/constants.js
+++ src/constants.js
@@ -8,3 +8,4 @@ export const COLORS = {
   transparentGray15: 'hsl(0deg 0% 50% / 0.15)',
   transparentGray35: 'hsl(0deg 0% 50% / 0.35)',
 };
+
