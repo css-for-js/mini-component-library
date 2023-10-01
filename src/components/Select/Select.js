7889aab6ecaeeb16428f6a97b8fc7aa15fe0ee80
Stephen Weiss <stephen.c.weiss@gmail.com>
2023-10-01 15:13:59 -0500

    select implementation

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,52 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+
+const InternalSelect = styled.select`
+  appearance: none;
+  padding: 12px 52px 12px 16px;
+
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+  width: ${p => p.displayedValue.length * 8 + 68 - 8}px;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+
+`
+
+const Wrapper = styled.div`
+  position: relative;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+    ${InternalSelect} {
+      color: ${COLORS.black};
+    }
+  }
+  `
+const IconWrapper = styled(Icon)`
+  position: absolute;
+  top: 8px;
+  left: ${p => p.displayedValue.length * 8 + 24}px;
+  pointer-events: none;
+`
+
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <InternalSelect
+        aria-label={label}
+        displayedValue={displayedValue}
+        value={value}
+        onChange={onChange}>
+        {children}
+      </InternalSelect>
+      <IconWrapper displayedValue={displayedValue} id={"chevron-down"} />
+    </Wrapper>
   );
 };
 
